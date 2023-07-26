5bc0c362bd77ce1f0a6361365bed033b8a9dad8c
obatayuki <yukiobata1@gmail.com>
2023-07-26 19:58:13 +0900

    Update confirm.py

--- confirm.py
+++ confirm.py
@@ -29,86 +29,87 @@ def preprocess(df, unused):
 
 df = preprocess(original, unused)
 
-#for index, row in df.iterrows():
-#  # 通し番号i番の抽選　
-#  id = row["ID"]
-#  password = row["パスワード"]
-
- 
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
- 
 
-# 予約ボタンクリック
-driver.get("https://www.pa-reserve.jp/eap-ri/rsv_ri/i/im-0.asp?KLCD=119999")
-reservation_button = driver.find_element(By.XPATH, "//*[text()='施設の予約']")
-reservation_button.click()
 
-# ログイン
-userid = "0000095667"
-password = "0517"
-
-user_to_fill = driver.find_element(By.XPATH, "//input[@name='txtUserCD']")
-user_to_fill.send_keys(userid)
-
-pass_to_fill = driver.find_element(By.XPATH, "//input[@name='txtPassword']")
-pass_to_fill.send_keys(password)
-
-ok_button = driver.find_element(By.XPATH, "//input[contains(@value,'ＯＫ')]")
-ok_button.click()
-
-# 所在地
-
-select_by_place = driver.find_element(By.XPATH, "//a[text()='所在地から検索／予約']")
-select_by_place.click()
-
-western_area = driver.find_element(By.XPATH, "//a[contains(text(),'西部エリア')]")
-western_area.click()
-
-proceed_button = driver.find_element(By.XPATH, "//input[contains(@value,'次へ')]")
-proceed_button.click()
-
-tokorozawa_park = driver.find_element(By.XPATH, "//a[contains(text(),'所沢航空記念公園')]")
-tokorozawa_park.click()
-# ここから確認用
-
-any_court = driver.find_element(By.XPATH, "//input[contains(@name,'rdo_SHISETU')]")
-any_court.click()
-
-ok_button = driver.find_element(By.XPATH, "//input[contains(@value,'ＯＫ')]")
-ok_button.click()
-
-# 有効期限切れ
-try:
-  driver.find_element(By.XPATH, "//form[contains(text(),  '利用者の有効期限が切れています')]")
-  print("この番号は有効期限が切れています。")
-  sys.exit()
-except NoSuchElementException:
-  pass
-
-while 1:
-  try: 
-    available_court = driver.find_element(By.XPATH, "//input[contains(@name, 'chkComa')]")
-    available_court.click()
-    #実際に予約するわけではなく、登録停止の確認のため
-    reservation_button = driver.find_element(By.XPATH, "//input[contains(@value,  '予約する')]")
-    reservation_button.click()
-    break
+def check_available(userid: str, password: str, driver) -> int:
+  # 使用不可→1を返す、使用可→0を返す
   
+  # 予約ボタンクリック
+  driver.get("https://www.pa-reserve.jp/eap-ri/rsv_ri/i/im-0.asp?KLCD=119999")
+  reservation_button = driver.find_element(By.XPATH, "//*[text()='施設の予約']")
+  reservation_button.click()
+  
+  user_to_fill = driver.find_element(By.XPATH, "//input[@name='txtUserCD']")
+  user_to_fill.send_keys(userid)
+  
+  pass_to_fill = driver.find_element(By.XPATH, "//input[@name='txtPassword']")
+  pass_to_fill.send_keys(password)
+  
+  ok_button = driver.find_element(By.XPATH, "//input[contains(@value,'ＯＫ')]")
+  ok_button.click()
+  
+  # 所在地
+  
+  select_by_place = driver.find_element(By.XPATH, "//a[text()='所在地から検索／予約']")
+  select_by_place.click()
+  
+  western_area = driver.find_element(By.XPATH, "//a[contains(text(),'西部エリア')]")
+  western_area.click()
+  
+  proceed_button = driver.find_element(By.XPATH, "//input[contains(@value,'次へ')]")
+  proceed_button.click()
+  
+  tokorozawa_park = driver.find_element(By.XPATH, "//a[contains(text(),'所沢航空記念公園')]")
+  tokorozawa_park.click()
+  # ここから確認用
+  any_court = driver.find_element(By.XPATH, "//input[contains(@name,'rdo_SHISETU')]")
+  any_court.click()
+  
+  ok_button = driver.find_element(By.XPATH, "//input[contains(@value,'ＯＫ')]")
+  ok_button.click()
+  
+  # 有効期限切れ
+  try:
+    driver.find_element(By.XPATH, "//form[contains(text(),  '利用者の有効期限が切れています')]")
+    return 1
+    sys.exit()
   except NoSuchElementException:
-    next_day = driver.find_element(By.XPATH, "//input[contains(@value,  '次日')]")
-    next_day.click()
+    pass
+  
+  while 1:
+    try: 
+      available_court = driver.find_element(By.XPATH, "//input[contains(@name, 'chkComa')]")
+      available_court.click()
+      
+      #実際に予約するわけではなく、登録停止の確認のため
+      reservation_button = driver.find_element(By.XPATH, "//input[contains(@value,  '予約する')]")
+      reservation_button.click()
+      break
+    
+    except NoSuchElementException:
+      # 次の日に遷移
+      next_day = driver.find_element(By.XPATH, "//input[contains(@value,  '次日')]")
+      next_day.click()
+  
+  # 利用停止の確認
+  try:
+    driver.find_element(By.XPATH, "//form[contains(text(),  '利用停止のため、予約することができません。')]")
+    # print("この番号は利用停止中です。")
+    return 1
+  except NoSuchElementException:
+    # print("この番号は使用可能です。")
+    return 0
 
-# 利用停止の確認
-try:
-  driver.find_element(By.XPATH, "//form[contains(text(),  '利用停止のため、予約することができません。')]")
-  print("この番号は利用停止中です。")
-except NoSuchElementException:
-  print("この番号は使用可能です。")
-#for element in driver.find_elements(By.XPATH, "/html/body"):
-#  print(element.text)
+availability = []
 
-#time.sleep(100)
+for index, row in df.iterrows():
+  # 通し番号i番の抽選　
+  userid = row["ID"]
+  password = row["パスワード"]
+  availability.append(check_available(userid, password, driver))
+df["利用可否(1:可, 0:不可)"] = availability
