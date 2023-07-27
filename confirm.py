6a62209ef68c40215154b36d9e114caed32c8165
obatayuki <yukiobata1@gmail.com>
2023-07-27 19:17:47 +0900

    Update confirm.py

--- confirm.py
+++ confirm.py
@@ -16,7 +16,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium.common.exceptions import NoSuchElementException
 
-if not utils.check_schedule_within_30_minutes():
+if utils.check_schedule_within_30_minutes()==0:
   print("毎月第2水曜22:30～翌8:00,毎週金曜3:00～3:30は利用できません。")
   exit(0)
 
