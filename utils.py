9be55187a398f36efe61c7baa8c8c1fb58b1f618
obatayuki <yukiobata1@gmail.com>
2023-07-27 18:33:12 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -46,37 +46,46 @@ def save_to_excel(df, file_name):
     # Save the Excel file
     wb.save(file_name)
 
-def check_schedule():
-    
+
+def check_schedule_within_30_minutes():
     # 日本の時刻
     timezone_utc_plus_9 = pytz.timezone('Asia/Tokyo')
-    currrent_time = datetime.now(timezone_utc_plus_9)
-    
-# 毎週金曜日のスケジュール
+    current_time = datetime.now(timezone_utc_plus_9)
+
+    # 毎週金曜日のスケジュール
     friday_schedule = {'day_of_week': 4, 'start_time': '03:00', 'end_time': '03:30'}
-    
+
     # 毎月第2水曜日のスケジュール
     second_wednesday = current_time.replace(day=15)
     while second_wednesday.weekday() != 2:  # 水曜日を探す
         second_wednesday += timedelta(days=1)
-        second_wednesday_schedule = {'day': second_wednesday.day, 'start_time': '22:30', 'end_time': '23:59'}
-    next_day_schedule = {'day': (second_wednesday+timedelta(days=1)).day, 'start_time': '00:00', 'end_time': '08:00'}
-    
-    # 現在時刻がスケジュール内かどうかをチェック
-    schedules = [friday_schedule, second_wednesday_schedule, next_day_schedule]
-    
-    for schedule in schedules:
-        start_time = datetime.strptime(schedule['start_time'], '%H:%M').time()
-        end_time = datetime.strptime(schedule['end_time'], '%H:%M').time()
+    second_wednesday_schedule = {'day': second_wednesday.day, 'start_time': '22:30', 'end_time': '23:59'}
+    next_day_schedule = {'day': (second_wednesday + timedelta(days=1)).day, 'start_time': '00:00', 'end_time': '08:00'}
 
-        if not (day := schedule.get("day")):
-            # 毎週金曜日について
-            day_of_week = schedule['day_of_week']
-        
-            if current_time.weekday() == day_of_week and start_time <= current_time.time() <= end_time:
-                return 0
-        else:
-            #第2水曜日の判定　
-            if current_time.day == day and start_time <= current_time.time() <= end_time:
-              return 0
-    return 1
+    # 現在時刻から30分後までをチェック
+    for _ in range(31):
+        # Check the current time and two schedules
+        if check_single_schedule(current_time, friday_schedule) or check_single_schedule(current_time, second_wednesday_schedule) or check_single_schedule(current_time, next_day_schedule):
+            return 1
+        # Increment current_time by 1 minute for the next iteration
+        current_time += timedelta(minutes=1)
+
+    return 0
+
+
+def check_single_schedule(current_time, schedule):
+    start_time = datetime.strptime(schedule['start_time'], '%H:%M').time()
+    end_time = datetime.strptime(schedule['end_time'], '%H:%M').time()
+
+    if not (day := schedule.get("day")):
+        # 毎週金曜日について
+        day_of_week = schedule['day_of_week']
+
+        if current_time.weekday() == day_of_week and start_time <= current_time.time() <= end_time:
+            return True
+    else:
+        # 第2水曜日の判定
+        if current_time.day == day and start_time <= current_time.time() <= end_time:
+            return True
+
+    return False
