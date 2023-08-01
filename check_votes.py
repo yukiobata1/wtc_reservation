2325491b218a0894acab4a7c64a0ce2b5a729ec6
obatayuki <yukiobata1@gmail.com>
2023-08-01 16:09:44 +0900

    Update check_votes.py

--- check_votes.py
+++ check_votes.py
@@ -67,8 +67,7 @@ court_list = [
 
 for court in court_list:
   # 各コートについて抽選数確認　
-  form = driver.find_element(By.XPATH, "//form")
-  children = form.find_elements(By.XPATH, '*')
+  children = driver.find_elements(By.XPATH, '//text()/html/body/form/*')
   for child in children:
     print("\nChild Element")
     print(child.get_attribute('outerHTML'))
