f539625032477c17e8326ae7b7d7ab965e84b62c
obatayuki <yukiobata1@gmail.com>
2023-08-02 13:25:06 +0900

    Update check_votes.py

--- check_votes.py
+++ check_votes.py
@@ -68,7 +68,7 @@ court_list = [
 
 for court in court_list:
   # 各コートについて抽選数確認
-  current_date = datetime.date.today()
+  current_date = date.today()
   next_month_date = current_date.replace(day=1) + datetime.timedelta(days=32)
   next_month_date = next_month_date.replace(day=1)
 
