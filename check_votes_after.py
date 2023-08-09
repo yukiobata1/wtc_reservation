ba619b0dc2d7ee1df55aebd534d89c9ffa032d68
obatayuki <yukiobata1@gmail.com>
2023-08-09 15:37:11 +0900

    Update check_votes_after.py

--- check_votes_after.py
+++ check_votes_after.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
   except FileNotFoundError:
     print("create a new exact_dest")
     exact_dest = pd.DataFrame([], columns = ["date", "court", "time_range", "通し番号", "userid", "password"])
-    exact_dest = exact_dest(["date", "court", "time_range", "通し番号", "userid", "password"])
+    exact_dest = exact_dest["date", "court", "time_range", "通し番号", "userid", "password"]
     
   try:
     exact_used_row = pd.read_csv(os.path.join(DATA_BASE, "exact_used_row.csv"))
