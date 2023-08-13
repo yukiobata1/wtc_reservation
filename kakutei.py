6f1c056a2217ad466f85f8f99c66ac66524700db
obatayuki <yukiobata1@gmail.com>
2023-08-13 19:00:18 +0900

    Update kakutei.py

--- kakutei.py
+++ kakutei.py
@@ -70,12 +70,13 @@ def single_kakutei(num, userid: str, password: str):
   if len(after_confirm) == 0:
     print(f"No entries after confirmation")
   else:
-    
     # 確定後の当選票
     for i, el in enumerate(after_confirm):
       # すでにcsv内に入ってなければ保存
       driver.find_elements(By.XPATH, "//input[@name='rdoYoyakuNO'][following-sibling::text()[1][contains(., '当選確定済')]]")[i].click()
-  
+      tmp_text = driver.find_elements(By.XPATH, "//input[@name='rdoYoyakuNO'][following-sibling::text()[1][contains(., '当選確定済')]]")[i].text
+      print(f"{tmp_text=}")
+      
       votes_won = pd.read_csv(os.path.join(DATA_BASE, "votes_won.csv"))[["date", "court", "time_range", "通し番号", "userid", "password", "予約申請番号"]]
   
       confirmation_button = driver.find_element(By.XPATH, "//input[contains(@value, '確認')]")
@@ -86,8 +87,7 @@ def single_kakutei(num, userid: str, password: str):
   
       driver.execute_script("window.history.go(-1)")
   
-      print(data)
-      print(f"{num=}, {userid=}, {password=}")
+      print(f"{data=}")
       
       to_add = pd.DataFrame({"date":[data["date"]], "court":[data["court"]], "time_range": [data["time_range"]], "通し番号": [num], "userid": [userid], "password": [password], "予約申請番号":[data["reservation_number"]]})
       votes_won = pd.concat([votes_won, to_add])
